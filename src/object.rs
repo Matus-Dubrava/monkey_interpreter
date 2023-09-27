244c87f749e2d4d33645a72831d4a5629e5fff74
Matus Dubrava <matusdubrava@gmail.com>
2023-09-27 09:37:24 +0200

    impl of basic object types

--- src/object.rs
+++ src/object.rs
@@ -1,11 +1,17 @@
 enum ObjectType {
     INTEGER_OBJ,
+    BOOLEAN_OBJ,
+    FLOAT_OBJ,
+    NULL_OBJ,
 }
 
 impl ToString for ObjectType {
     fn to_string(&self) -> String {
         match self {
             Self::INTEGER_OBJ => "INTEGER".to_string(),
+            Self::BOOLEAN_OBJ => "BOOLEAN".to_string(),
+            Self::FLOAT_OBJ => "FLOAT".to_string(),
+            Self::NULL_OBJ => "NULL".to_string(),
         }
     }
 }
@@ -26,5 +32,45 @@ impl Object for Integer {
 
     fn get_type(&self) -> ObjectType {
         return ObjectType::INTEGER_OBJ;
+    }
+}
+
+struct Boolean {
+    value: bool,
+}
+
+impl Object for Boolean {
+    fn get_type(&self) -> ObjectType {
+        ObjectType::BOOLEAN_OBJ
+    }
+
+    fn inspect(&self) -> String {
+        format!("{}", self.value)
+    }
+}
+
+struct Float {
+    value: f64,
+}
+
+impl Object for Float {
+    fn get_type(&self) -> ObjectType {
+        ObjectType::FLOAT_OBJ
+    }
+
+    fn inspect(&self) -> String {
+        format!("{}", self.value)
+    }
+}
+
+struct Null {}
+
+impl Object for Null {
+    fn get_type(&self) -> ObjectType {
+        ObjectType::NULL_OBJ
+    }
+
+    fn inspect(&self) -> String {
+        "null".to_string()
     }
 }
